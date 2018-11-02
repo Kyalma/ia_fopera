6635cf3cb273d2a69f38216b6898150869f1b484
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-11-02 16:39:36 +0100

    updating turns

--- parser/infos.py
+++ parser/infos.py
@@ -11,11 +11,8 @@ suspects = {
     'gris': characters.Grey()
 }
 
-def pawns_position():
-    pass
-
 def ghost_color():
-    with open('../0/infos.txt', 'r') as fhandler:
+    with open('../1/infos.txt', 'r') as fhandler:
         data = fhandler.readline()
     *__ , color = data.split()
     print(f"Le fantome est {color}")
@@ -23,18 +20,23 @@ def ghost_color():
 def current_turn_infos(role):
     with open(f'{role}/infos.txt', 'r') as fhandler:
         turns = fhandler.read().split('**************************\n')
-        if role == 0:
-            turns.pop(0)
-        current_turn = turns[-1].split('\n')
-        events = current_turn[0]
-        status = current_turn[1].split()
-        return events, status
+    if role == 0:
+        turns.pop(0)
+    current_turn = turns[-1].split('\n')
+    events = current_turn[0]
+    status = current_turn[1].split()
+    subs_current = turns[-1].split('****\n')
+    subs_current.pop(0)
+    for sub in subs_current:
+        for line in sub.split('\n'):
+            if line.startswith("NOUVEAU PLACEMENT : "):
+                status.append(line[20:])
+    return events, status
 
 
 if __name__ == "__main__":
     events, status = current_turn_infos(0)
     for suspect in status:
         suspects[suspect[:suspect.find('-')]].update(suspect)
-    for color in suspects:
-        print(suspects[color], end='\n')
-
+    for color, suspect in suspects.items():
+        print(suspect, end='\n')
