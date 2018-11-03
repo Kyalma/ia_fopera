2d0cbca6375198af184c14764b2a66690fed47f8
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-11-03 14:40:24 +0100

    suspects

--- player/joueur.py
+++ player/joueur.py
@@ -2,6 +2,16 @@
 class   Joueur():
     def __init__(self, id):
         self.id = id
+        self.suspects = {
+            'marron': characters.Brown(),
+            'rose': characters.Pink(),
+            'noir': characters.Black(),
+            'rouge': characters.Red(),
+            'bleu': characters.Blue(),
+            'blanc': characters.White(),
+            'violet': characters.Violet(),
+            'gris': characters.Grey()
+        }
 
     def lancer(self):
         raise NotImplementedError()
