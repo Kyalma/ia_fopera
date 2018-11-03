cf3fe3d253449f3a8a4cd33074866c4c99e4dac4
william <william.moustrou@lsmed.com>
2018-11-03 20:00:47 +0100

    [ADD] question parsing: powers done.

--- parser/read_question.py
+++ parser/read_question.py
@@ -1,16 +1,42 @@
-def read_question(nb):
-    with open(f"../{nb}/questions.txt", 'r') as filehandler:
-        data = filehandler.read()
-    qdata = data.split()
-    if qdata[0] == 'Voulez-vous':
-        print('0')
-        return 0
-    elif qdata[0] == 'positions':
-        print(data[data.find('{')+1:data.find('}')].split(', ')) 
-        return 1, data[data.find('{')+1:data.find('}')].split(', ')
-    else:
-        print(data[data.find('[')+1:data.find(']')].split(', '))
-        return 2, data[data.find('[')+1:data.find(']')].split(', ')        
+from enum import Enum 
 
+class TYPE(Enum):
+    POWER = 0
+    DRAW = 1
+    POSITION = 2
+    PURPLE_POWER = 3
+    GRAY_POWER = 4
+    BLUE_POWER = 5
+    BLUE_POWER_EXIT = 6
+class Question:
+    def __init__(self, file):
+        self.file = file
+        self.type = None
+        self.args = None
+    def read(self):
+        with open(self.file, 'r') as filehandler:
+            data = filehandler.read()
+        qdata = data.split()
+        if qdata[0] == 'Voulez-vous':
+            print('0')
+            self.type = TYPE.POWER
+        elif qdata[0] == 'positions':
+            self.type = TYPE.POSITION
+            self.args = data[data.find('{')+1:data.find('}')].split(', ') 
+        elif qdata[0] == 'Tuiles':
+            self.type = TYPE.DRAW
+            self.args = data[data.find('[')+1:data.find(']')].split(', ')        
+        elif qdata[0] == 'Quelle' and qdata[1] == 'salle' and qdata[2] == 'bloquer':
+            self.type = TYPE.BLUE_POWER
+        elif qdata[0] == 'Quelle' and qdata[1] == 'sortie':
+            self.type = TYPE.BLUE_POWER_EXIT
+            self.args = data[data.find('{')+1:data.find('}')].split(', ')
+        elif qdata[0] == 'Quelle' and qdata[1] == 'salle' and qdata[2] == 'obscurcir':
+            self.type = TYPE.GRAY_POWER
+        else:
+            self.type = TYPE.PURPLE_POWER 
 if __name__=="__main__":
-    read_question(3)
+    question = Question('../0/questions.txt')
+    question.read()
+    print(question.type)
+    print(question.args)
