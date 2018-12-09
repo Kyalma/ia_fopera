401ad49d02417f2083c091d32bf02389b71274c9
Léo Poulin <leoraphael.poulin@epitech.eu>
2018-12-09 15:04:46 +0100

    big parser for all turns

--- parser/infos.py
+++ parser/infos.py
@@ -1,5 +1,6 @@
 import re
 from time import sleep
+from parser.read_question import Question, Type
 
 import game.characters as characters
 
@@ -46,3 +47,45 @@ def current_turn_infos(role):
                         status[i] = new_pos
             # Add new features here for parsing each turn
     return events, status
+
+def all_turns(role: int) -> list:
+    all_turns_info = list()
+    question = Question(None)
+    data = read_file(f'{role}/infos.txt')
+    turns = data.split('**************************\n')
+    turns.pop(0)
+    for big_turn in turns:
+        turn_meta = big_turn.split('\n', 2)
+        big_events = parse_events(turn_meta[0])
+        suspects = turn_meta[1].split()
+        sub_turns = big_turn.split('****\n')
+        sub_turns.pop(0)
+        for sub_turn in sub_turns:
+            turn_info = dict()
+            turn_info['tour'] = big_events[0]
+            turn_info['score'] = big_events[1]
+            turn_info['ombre'] = big_events[2]
+            turn_info['bloqué'] = big_events[3]
+            turn_info['suspects'] = suspects.copy()
+            for line in sub_turn.split('\n'):
+                if line.startswith('QUESTION : '):
+                    question.parse_question(line[11:])
+                    if question.type == Type.DRAW:
+                        next_value = 'personnage joué'
+                    elif question.type == Type.POSITION:
+                        next_value = 'position'
+                    elif question.type == Type.POWER:
+                        next_value = 'pouvoir utilisé'
+                    else:
+                        next_value = 'autre'
+                if line.startswith('REPONSE INTERPRETEE : '):
+                    turn_info[next_value] = line[22:]
+                if line.startswith('NOUVEAU PLACEMENT : '):
+                    nouveau_placement = line[20:]
+                    for i, suspect in enumerate(suspects):
+                        if suspect.startswith(nouveau_placement[:nouveau_placement.find('-')]):
+                            suspects[i] = nouveau_placement
+            all_turns_info.append(turn_info)
+            pass
+        pass
+    return all_turns_info
